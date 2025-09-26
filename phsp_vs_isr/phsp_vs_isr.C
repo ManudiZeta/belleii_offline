a539f897d1b2e3ea74ef0e31a61aadd0c4ca49cf
Emanuele Zanusso <afteremanuele@gmail.com>
2025-09-26 17:31:03 +0200

    ridisposizione

--- phsp_vs_isr/phsp_vs_isr.C
+++ phsp_vs_isr/phsp_vs_isr.C
@@ -7,12 +7,12 @@
 #include "TLegend.h"
 #include "TStyle.h"
 
-void phsp_vs_isr()
+void gamma_phsp_vs_isr()
 {
     
     gStyle->SetOptStat(0);
-    string in_1 = "../root_file/isr/isr_list.root";
-    string in_2 = "../root_file/nbar_recoil/phsp_gamma_out.root";
+    string in_1 = "../../root_file/isr/isr_list.root";
+    string in_2 = "../../root_file/nbar_recoil/phsp_gamma_out.root";
     
     ifstream in_f1(in_1);
     ifstream in_f2(in_2);
@@ -25,8 +25,8 @@ void phsp_vs_isr()
     in_f1.close();
     in_f2.close();
     
-    TFile myf_1("../root_file/isr/isr_list.root");
-    TFile myf_2("../root_file/nbar_recoil/phsp_gamma_out.root");
+    TFile myf_1("../../root_file/isr/isr_list.root");
+    TFile myf_2("../../root_file/nbar_recoil/phsp_gamma_out.root");
     
     TTree *tree_1 = (TTree*)myf_1.Get("tree");
     TTree *tree_2 = (TTree*)myf_2.Get("tree");
@@ -55,18 +55,82 @@ void phsp_vs_isr()
     cout<<"NEntries_1 = "<<histo1->GetEntries()<<endl;
     cout<<"NEntries_2 = "<<histo2->GetEntries()<<endl;
     
-    TLegend *leg = new TLegend(0.5,0.6,0.78,0.78);
+    TLegend *leg = new TLegend(0.6,0.6,0.78,0.78);
     leg->AddEntry(histo1," ISR ","l");
     leg->AddEntry(histo2," PHSP ","l");
     
-    TCanvas *tela = new TCanvas("tela", "tela",800,600);
+    TCanvas *tela = new TCanvas("tela", "tela");
     
     histo1->DrawCopy("HIST");
     histo2->DrawCopy("HIST SAME");
     leg->Draw("SAME");
     
     tela->Update();
-    tela->SaveAs("phsp_vs_isr_gammaList.pdf");
+    tela->SaveAs("images/phsp_vs_isr_gammaList.pdf");
+    
+    
+}
+
+void nbar_phsp_vs_isr()
+{
+    
+    gStyle->SetOptStat(0);
+    string in_1 = "../../root_file/isr/isr_list_n.root";
+    string in_2 = "../../root_file/nbar_recoil/phsp_n_out.root";
+    
+    ifstream in_f1(in_1);
+    ifstream in_f2(in_2);
+    
+    if(!in_f1 | !in_f2)
+    {
+        cout<<"Selected root file doesn't exist. Please, generate it. \n";
+        return;
+    }
+    in_f1.close();
+    in_f2.close();
+    
+    TFile myf_1("../../root_file/isr/isr_list_n.root");
+    TFile myf_2("../../root_file/nbar_recoil/phsp_n_out.root");
+    
+    TTree *tree_1 = (TTree*)myf_1.Get("tree");
+    TTree *tree_2 = (TTree*)myf_2.Get("tree");
+    
+    TCanvas *c1 = new TCanvas("c1", "c1",800,600);
+    
+    tree_1->Draw("theta>>histo1(50,0,3.2)","mcPDG == -2112","goff");
+    tree_2->Draw("theta >> histo2(50,0,3.2)","genMotherPDG == 300553","goff");
+    
+    delete c1;
+    
+    TH1 *histo1 = (TH1D*)gDirectory->Get("histo1");
+    TH1 *histo2 = (TH1D*)gDirectory->Get("histo2");
+    
+    
+    histo1->Scale(1.0 / histo1->Integral());
+    histo2->Scale(1.0 / histo2->Integral());
+    
+    histo1->SetLineColor(kBlue);
+    histo2->SetLineColor(kRed);
+    
+    histo1->GetXaxis()->SetTitle("#theta [rad]");
+    histo1->GetYaxis()->SetTitle("counts []");
+    histo1->SetTitle("#bar{n} list from MC");
+    
+    cout<<"NEntries_1 = "<<histo1->GetEntries()<<endl;
+    cout<<"NEntries_2 = "<<histo2->GetEntries()<<endl;
+    
+    TLegend *leg = new TLegend(0.6,0.6,0.78,0.78);
+    leg->AddEntry(histo1," ISR ","l");
+    leg->AddEntry(histo2," PHSP ","l");
+    
+    TCanvas *tela = new TCanvas("tela", "tela");
+    
+    histo1->DrawCopy("HIST");
+    histo2->DrawCopy("HIST SAME");
+    leg->Draw("SAME");
+    
+    tela->Update();
+    tela->SaveAs("images/phsp_vs_isr_nList.pdf");
     
     
 }
