26edc3c1b2e7f5c69ee8adb28799f0d5346485ae
Emanuele Zanusso <afteremanuele@gmail.com>
2025-11-15 15:08:25 +0100

    Delta P in g_vs_n

--- g_vs_n/g_vs_n_list.C
+++ g_vs_n/g_vs_n_list.C
@@ -54,8 +54,115 @@ void g_vs_n(bool choice)
     
     TCanvas *c1 = new TCanvas("c1", "c1",800,600);
     
-    tree_1->Draw("nbar_clusterE1E9>>histo1(100,0,1)","","goff");
-    tree_2->Draw("nbar_clusterE1E9>>histo2(100,0,1)","","goff");
+    tree_1->Draw("vpho_r_pRecoil-nbar_mcP>>histo1(100,-1,1)","nbar_mcPDG == -2112","goff");
+    tree_2->Draw("vpho_r_pRecoil-nbar_mcP>>histo2(100,-1,1)","nbar_mcPDG == -2112","goff");
+    
+    delete c1;
+    
+    TH1 *histo1 = (TH1D*)gDirectory->Get("histo1");
+    TH1 *histo2 = (TH1D*)gDirectory->Get("histo2");
+    
+    double mean_1 = histo1->GetMean();
+    double devstd_1 = histo1->GetStdDev();
+    double entries_1 = histo1->GetEntries();
+    
+    double mean_2 = histo2->GetMean();
+    double devstd_2 = histo2->GetStdDev();
+    double entries_2 = histo2->GetEntries();
+    
+    
+    cout<<"entries_1 = "<<entries_1<<"|mean_1 = "<<mean_1<<"|devstd_1 = "<<devstd_1<<endl;
+    cout<<"entries_2 = "<<entries_2<<"|mean_2 = "<<mean_2<<"|devstd_2 = "<<devstd_2<<endl;
+    //histo1->Scale(1.0 / histo1->Integral());
+    //histo2->Scale(1.0 / histo2->Integral());
+ 
+    
+    double max1 = histo1->GetBinContent(histo1->GetMaximumBin());
+    cout<<"max1 = "<<max1<<endl;
+    double max2 = histo2->GetBinContent(histo2->GetMaximumBin());
+    cout<<"max2 = "<<max2<<endl;
+    double max = (max1 > max2) ? max1 : max2;
+    cout<<"max = "<<max<<endl;
+    
+    histo1->SetMaximum(max + 0.1*max);
+    histo2->SetMaximum(max + 0.1*max);
+    
+    histo1->SetLineColor(kBlue);
+    histo2->SetLineColor(kRed);
+    
+    histo1->GetXaxis()->SetTitle("#DeltaP []");
+    histo1->GetYaxis()->SetTitle("counts []");
+    
+    histo1->SetTitle("#DeltaP comparison in n e g hp");
+    
+    cout<<"NEntries_1 = "<<histo1->GetEntries()<<endl;
+    cout<<"NEntries_2 = "<<histo2->GetEntries()<<endl;
+    
+    TLegend *leg = new TLegend(0.6,0.6,0.78,0.78);
+    leg->AddEntry(histo1,"n list","l");
+    leg->AddEntry(histo2,"#gamma list","l");
+    
+    TCanvas *tela = new TCanvas("tela", "tela");
+    
+    histo1->DrawCopy("HIST");
+    histo2->DrawCopy("HIST SAME");
+    leg->Draw("SAME");
+    
+    tela->Update();
+    if (choice == 0)
+    {
+        tela->SaveAs("images/isr_g_vs_n_deltaP.pdf");
+    }
+    
+    else
+    {
+        tela->SaveAs("images/isr_g_vs_n_deltaP_kin.pdf");
+    }
+    
+}
+
+void nosel_vs_sel(bool choice)
+{
+    gStyle->SetOptStat(0);
+    string in_1, in_2;
+    TFile *myf_1 = nullptr;
+    TFile *myf_2 = nullptr;
+    
+    if (choice == 0)
+    {
+        in_1 = "../../../vpho_std_isr_n_REC.root";
+    }
+    
+    else
+    {
+        in_1 = "../../../vpho_std_isr_n_REC_kin.root";
+    }
+    
+    ifstream in_f1(in_1);
+    
+    if(!in_f1)
+    {
+        cout<<"Selected root file doesn't exist. Please, generate it. \n";
+        return;
+    }
+    in_f1.close();
+    
+    if (choice == 0)
+    {
+        myf_1 = new TFile("../../../vpho_std_isr_n_REC.root");
+    }
+    
+    else
+    {
+        myf_1 = new TFile("../../../vpho_std_isr_n_REC_kin.root");
+    }
+    
+    TTree *tree_1 = (TTree*)myf_1->Get("tree");
+    
+    TCanvas *c1 = new TCanvas("c1", "c1",800,600);
+    
+    tree_1->Draw("nbar_clusterNHits>>histo1(100,0,70)","","goff");
+    tree_1->Draw("nbar_clusterNHits>>histo2(100,0,70)","nbar_mcPDG == -2112","goff");
     
     delete c1;
     
@@ -79,17 +186,17 @@ void g_vs_n(bool choice)
     histo1->SetLineColor(kBlue);
     histo2->SetLineColor(kRed);
     
-    histo1->GetXaxis()->SetTitle("clusterE1E9 []");
+    histo1->GetXaxis()->SetTitle("clusterNHits []");
     histo1->GetYaxis()->SetTitle("counts []");
     
-    histo1->SetTitle("clusterE1E9 comparison in n e g hp");
+    histo1->SetTitle("clusterNHits comparison in not_sel  e sel");
     
     cout<<"NEntries_1 = "<<histo1->GetEntries()<<endl;
     cout<<"NEntries_2 = "<<histo2->GetEntries()<<endl;
     
     TLegend *leg = new TLegend(0.6,0.6,0.78,0.78);
-    leg->AddEntry(histo1,"n list","l");
-    leg->AddEntry(histo2,"#gamma list","l");
+    leg->AddEntry(histo1,"nbar_mcPDG = all","l");
+    leg->AddEntry(histo2,"nbar_mcPDG = -2112","l");
     
     TCanvas *tela = new TCanvas("tela", "tela");
     
@@ -100,12 +207,12 @@ void g_vs_n(bool choice)
     tela->Update();
     if (choice == 0)
     {
-        tela->SaveAs("images/isr_g_vs_n_clusterE1E9.pdf");
+        tela->SaveAs("images/isr_nosel_vs_sel_clusterNHits.pdf");
     }
     
     else
     {
-        tela->SaveAs("images/isr_g_vs_n_clusterE1E9_kin.pdf");
+        tela->SaveAs("images/isr_nosel_vs_sel_clusterNHits_kin.pdf");
     }
     
 }
